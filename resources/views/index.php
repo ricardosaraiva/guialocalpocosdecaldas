28e3971f58e48b61cd00dc65e74694cb498008b3
ricardo <ricardobboy_@hotmail.com>
2017-08-22 00:24:39 -0300

    Foi adicionado no layout um modelo para cadastro de empresas

--- resources/views/index.php
+++ resources/views/index.php
@@ -11,13 +11,63 @@
 </head>
 <body>
 
+
+	<div class="modal fade" id="novaEmpresa">
+	  <div class="modal-dialog modal-lg" role="document">
+	    <div class="modal-content">
+	      <div class="modal-header">
+	        <h5 class="modal-title">Adicionar nova empresa</h5>
+	        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+	          <span aria-hidden="true">&times;</span>
+	        </button>
+	      </div>
+	      <div class="modal-body">
+	        <div class="row">
+	        	<div class="col-md-6">
+	        		<label for="razao">Razão Social</label>
+	        		<input type="text" class="form-control" id="razao">
+	        	</div>
+
+				<div class="col-md-6">
+	        		<label for="fantasia">Nome Fantasia</label>
+	        		<input type="text" class="form-control" id="fantasia">
+	        	</div>	        	
+	        </div>
+ 			<div class="row">
+	        	<div class="col-md-4">
+	        		<label for="razao">CNPJ</label>
+	        		<input type="text" class="form-control" id="razao">
+	        	</div>
+
+				<div class="col-md-4">
+	        		<label for="fantasia">Telefone</label>
+	        		<input type="text" class="form-control" id="telefone">
+	        	</div>	        	
+
+				<div class="col-md-4">
+	        		<label for="fantasia">E-mail</label>
+	        		<input type="text" class="form-control" id="telefone">
+	        	</div>	        		        	
+	        </div>	        
+	      </div>
+	      <div class="modal-footer">
+	        <button type="button" class="btn btn-success">Enviar</button>
+	      </div>
+	    </div>
+	  </div>
+	</div>
+
 	<button onclick="toggleFullScreen()" class="btn btn-success fullscreen">
 		<i class="fa fa-arrows-alt"></i>
 	</button>
 
-	<button onclick="filtros()" id="filtros" class="btn btn-info filtros">
+	<button onclick="filtros()" id="filtros" class="btn btn-success filtros">
 		<i class="fa fa-search"></i>
 	</button>	
+
+	<button  data-toggle="modal" href='#novaEmpresa' class="btn btn-success novaEmpresa">
+		<i class="fa fa-plus"></i>
+	</button>		
 
 	<div id="formulario" class="formulario">
 
