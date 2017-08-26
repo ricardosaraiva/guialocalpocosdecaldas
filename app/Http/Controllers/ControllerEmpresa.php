b77601f1c089c4ab9a104184c34d118e46c47f33
ricardo <ricardobboy_@hotmail.com>
2017-08-25 23:27:40 -0300

    Configurado para roda na linha de comanda para atualizar o banco de dados

--- app/Http/Controllers/ControllerEmpresa.php
+++ app/Http/Controllers/ControllerEmpresa.php
@@ -3,6 +3,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Model\Empresa;
+use GuzzleHttp\Exception\RequestException;
 
 class ControllerEmpresa extends Controller {
 
