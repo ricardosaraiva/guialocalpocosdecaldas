58bfe77e5226cad4f98c6a4df41bfd1bda3bb9da
ricardo <ricardobboy_@hotmail.com>
2017-08-26 17:09:14 -0300

    Foi adicionado um modal de alerta para quando nenhum registro for encontrado!

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -150,8 +150,27 @@
 
 	<div id="map" class="map"></div>
 
+	<div class="modal fade" id="modalErro" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+	  <div class="modal-dialog" role="document">
+	    <div class="modal-content">
+	      <div class="modal-header">
+	        <h5 class="modal-title" id="exampleModalLabel"><i class="fa fa-exclamation"></i> Alerta</h5>
+	        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+	          <span aria-hidden="true">&times;</span>
+	        </button>
+	      </div>
+	      <div class="modal-body">
+	        Nenhum registro encontrado!
+	      </div>
+	    </div>
+	  </div>
+	</div>
+
 </body>
 </html>
+
+
+
 
 <script type="text/javascript" src="{{url('/assets/js/loader.js')}}"></script>
 <script src="{{url('/assets/js/jquery-3.1.1.slim.min.js')}}"></script>
@@ -365,8 +384,10 @@ $vm  = new Vue({
 			return false;   		
     	} 
     	
+
     	document.getElementById('menuGraficoPeriodo').style.display = 'none'; 
     	document.getElementById('menuGraficoPeriodo').style.display = 'none';
+    	$('#modalErro').modal('show')
     	   	
     }
 
